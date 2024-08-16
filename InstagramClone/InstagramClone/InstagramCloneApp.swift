fae18b49762d4a82bd603045c0c0b9a93abbd49a
Taro Altricher <ta029@hdm-stuttgart.de>
2024-08-16 16:16:43 +0200

    project setup

---
+++ InstagramClone/InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Taro Altrichter on 16.08.24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
